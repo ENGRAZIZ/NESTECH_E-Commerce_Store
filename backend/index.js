da6bd2b97049fb6a577780a3ae680d8234c32afb
AZIZ UR REHMAN <eengr.aziz.awan@gmail.com>
2024-10-18 13:00:53 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const connectDB = require('./config/db');
