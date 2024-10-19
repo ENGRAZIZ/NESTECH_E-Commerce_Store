425dbcf0f2fd80e5cd1c8ac08a6a63ae411061ab
AZIZ UR REHMAN <eengr.aziz.awan@gmail.com>
2024-10-19 23:43:26 +0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 
 async function connectDB(){
     try{
-        await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://AzizUr:tQARR96s73VqC4r6@nestech.ygoo0.mongodb.net/NESTECH?retryWrites=true&w=majority&appName=NESTECH')
+        await mongoose.connect('mongodb+srv://AzizUr:tQARR96s73VqC4r6@nestech.ygoo0.mongodb.net/NESTECH?retryWrites=true&w=majority&appName=NESTECH')
     }catch(err){
         console.log(err)
     }
