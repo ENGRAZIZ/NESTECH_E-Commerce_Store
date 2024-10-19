176cf81173130e5577bc4263c01b1d12196275a1
AZIZ UR REHMAN <eengr.aziz.awan@gmail.com>
2024-10-19 23:10:13 +0500

    123

--- README.md
+++
@@ -1,2 +0,0 @@
-# NESTECH_E-Commerce_Store
-Pakistan's No.1 Store
--- frontend/src/common/role.js
+++
@@ -1,6 +0,0 @@
-const ROLE = {
-    ADMIN : "ADMIN", // admin   only                    
-    GENERAL : "GENERAL", // general only          
-}
-
-export default ROLE
